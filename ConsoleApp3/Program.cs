71c6d57678fd561cc21c1352effcfe7f735f73b3
rajputmeet10@gmail.com <rajputmeet10@gmail.com>
2021-03-01 06:54:09 +0530

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace meetrajput
+{
+    class meet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
